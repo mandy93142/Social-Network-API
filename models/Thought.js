fc011fad51b431d6b9bddd55817eef0a9fc779b7
Chiao-Han Chang <mandy93142@gmail.com>
2024-02-22 03:58:31 +0800

    added dateFormat.js

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+
+    },
+    createdAt: {
+
+    },
+    username:{
+
+    },
+    reactions: []
+
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
+
+});
+
+thoughtSchema.virtual("reactionCount").get(function(){
+    return this.reactions.length;
+})
+
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
